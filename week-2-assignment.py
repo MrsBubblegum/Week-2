431493480a626b2373304130d82fd80eacd7964e
MrsBubblegum <cherozade@gmail.com>
2025-03-26 15:45:38 +0200

    Python week 2 assignment

--- week-2-assignment.py
+++ week-2-assignment.py
@@ -7,24 +7,18 @@ my_list.append(20)
 my_list.append(30)
 my_list.append(40)
 
-print(my_list)
-
 #Question 3
 my_list.insert(1,15)
-print(my_list)
 
 #Question 4
 extended_list = [50,60,70]
 my_list.extend(extended_list)
-print("after append", my_list)
 
 #Question 5
 my_list.pop()
-print ("after pop", my_list)
 
 #Question 6
 my_list.sort()
-print("after sort", my_list)
 
 #Question 7
 index = my_list.index(30)
